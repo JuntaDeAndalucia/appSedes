8845b068fb8c6e591dc4f0ded6cc2523a41cdd9a
sigcorporativo-ja <alvaro.zabala@juntadeandalucia.es>
2015-06-25 11:48:12 +0200

    Primer commit

---
+++ js/config.js
@@ -0,0 +1,6 @@
+var urlMapea = "http://mapea-sigc.juntadeandalucia.es";
+var contextos = "wmcfile=http://www.ideandalucia.es/services/www/ise/mapcontext/ise_callejero.xml*Callejero,http://www.ideandalucia.es/services/www/ise/mapcontext/ise_hibrido.xml*Hibrido,http://www.ideandalucia.es/services/www/ise/mapcontext/ise_satelite.xml*Satelite";
+var layers = "";
+var urlGeosearch = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sedes/search";
+var extra = "operations=searchstreet";
+
